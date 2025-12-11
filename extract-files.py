ab9eab306c0c82c8eb4861343ea8abe3c442d255
Bruno Martins <bgcngm@gmail.com>
2025-12-11 23:50:49 +0000

    onyx: Use a vendor copy of libtensorflow_lite_c
    This library is present since BP4A, but not available to vendor.
    Workaround elfchecks failure by using the prebuilt under a modified
    name.
    
    Change-Id: I653615bd3cb0e3dd8536d9cfd80841d500adf386

--- extract-files.py
+++ extract-files.py
@@ -217,6 +217,16 @@ blob_fixups: blob_fixups_user_type = {
             'libbluetooth_audio_session_aidl_prebuilt.so'
     ),
 
+    (
+        'vendor/lib64/libVoiceSdk.so',
+        'vendor/lib64/libcapiv2uvvendor.so',
+        'vendor/lib64/liblistensoundmodel2vendor.so'
+    ): blob_fixup()
+        .replace_needed(
+            'libtensorflowlite_c.so',
+            'libtensorflowlite_c_vendor.so'
+    ),
+
     'vendor/lib64/libultrahdr_prebuilt.so': blob_fixup()
         .replace_needed(
             'libjpegdecoder.so',
