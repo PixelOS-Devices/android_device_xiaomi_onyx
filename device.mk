1149d38760b1d8a9eb4b1bfd5687e5c90562db98
AdarshGrewal <adarshgrewal@gmail.com>
2026-01-31 19:18:27 +0530

    onyx: Rename dummy hypsys service
    Change-Id: Ic491a247934c0a9b796a353a53e037c3da1b6a26

--- device.mk
+++ device.mk
@@ -180,7 +180,7 @@ PRODUCT_COPY_FILES += \
 
 # Hypsy
 PRODUCT_PACKAGES += \
-    xiaomi.system.hypsys.common-service
+    xiaomi.system.hypsys.common-service-dummy
 
 # Health
 PRODUCT_PACKAGES += \
